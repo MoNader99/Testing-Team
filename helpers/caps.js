6b73ae893acca56fb3b3822413cff758b8c17caf
Maram Hosni <46975628+Maram31@users.noreply.github.com>
2020-04-09 22:46:49 +0200

    Update caps.js

--- helpers/caps.js
+++ helpers/caps.js
@@ -22,10 +22,18 @@ exports.androidAS = {
   appActivity: undefined // will be set later
 };
 
+exports.androidVirtualMM = {
+  browserName: '',
+  platformName: 'Android',
+  platformVersion: '9',
+  deviceName: 'emulator-5554',
+  appActivity: undefined // will be set later
+};
+
 exports.androidMM = {
   browserName: '',
   platformName: 'Android',
-  platformVersion: '6',
-  deviceName: 'Galaxy Grand Prime+',
+  platformVersion: '9',
+  deviceName: 'ce08171898c76917057e',
   appActivity: undefined // will be set later
 };
