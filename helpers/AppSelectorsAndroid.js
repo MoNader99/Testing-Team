1c79fd246462d5af583b1b2a09563ec67983b47c
AyaSameh <aya.sameh.99@gmail.com>
2020-04-11 17:09:24 +0200

    Updating Search Page

--- helpers/AppSelectorsAndroid.js
+++ helpers/AppSelectorsAndroid.js
@@ -40,6 +40,7 @@ exports.SearchArabicResult="/hierarchy/android.widget.FrameLayout/android.widget
 exports.SearchRAndBResult="/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.view.ViewGroup/android.widget.GridView/android.view.ViewGroup[6]/android.widget.TextView";
 exports.FirstSearchResultText="/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[2]/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.view.ViewGroup[1]/android.widget.TextView[1]";
 
+
 //////////////////////////////////
 ///////EditProfileSelectors//////
 exports.SettingButtonID="com.example.spotify:id/settingsImageview";
