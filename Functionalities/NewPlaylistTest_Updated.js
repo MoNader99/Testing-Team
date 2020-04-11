484614887306c68e13e46ede8216cf89fb64f131
MaramHosni <maram311999@hotmail.com>
2020-04-11 22:55:14 +0200

    Update NewPlaylistTest_Updated.js

--- Functionalities/NewPlaylistTest_Updated.js
+++ Functionalities/NewPlaylistTest_Updated.js
@@ -40,9 +40,64 @@ describe('Login on Spotify website to use webplayer', function() {
         await driver.sleep(3000);
     });
 
+    it('Try cancel button for create new playlist', async function() {
+
+        //Try cancel button
+        var text;
+        text = await driver.findElement(By.xpath(Selectors.NewPlaylistPopUpXpath)).getText();
+        expect(text).to.equal('Playlist Name');
 
 
 
+        //await driver.findElement(By.id(Selectors.NewPlaylistPopUpID.));
+
+        await driver.findElement(By.xpath(Selectors.CancelCreateNewPlaylistXpath)).click(); 
+        await driver.sleep(3000);
+
+
+    });
+
+
+    it('Try x button for create new playlist', async function() {
+
+        //Try x button
+
+        await driver.findElement(By.xpath(Selectors.ExitCreateNewPlaylistXpath)).click(); 
+        await driver.sleep(3000);
+
+    });
+
+
+    it('Try create new playlist with existing name', async function() {
+        //Enter playlist name
+        await driver.findElement(By.id(Selectors.InputNewPlaylistNameID)).sendKeys('Pop');
+        await driver.sleep(3000);
+
+        await driver.findElement(By.xpath(Selectors.CreateNewPlaylistXpath)).click(); 
+        await driver.sleep(3000);
+
+    });
+
+
+    it('Try create new playlist with no name', async function() {
+        await driver.findElement(By.xpath(Selectors.CreateNewPlaylistXpath)).click(); 
+        await driver.sleep(3000);
+
+
+
+    });
+
+
+    it('Try create new playlist with new name', async function() {
+        await driver.findElement(By.id(Selectors.InputNewPlaylistNameID)).sendKeys('Jazz');
+        await driver.sleep(3000);
+
+        await driver.findElement(By.xpath(Selectors.CreateNewPlaylistXpath)).click(); 
+        await driver.sleep(3000); 
+
+    });
+
+    
 
     it('Try create new playlist then delete it', async function() {
         await driver.findElement(By.id(Selectors.InputNewPlaylistNameID)).sendKeys('Rock');
